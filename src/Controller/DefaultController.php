fe62f1c66546bc3cbd7e9b4ced594cfb67a94c72
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2020-11-30 20:09:40 +0100

    add design, finish quest to link episodes and seasons

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
      */
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'programs' => $programRepository->findAll()
+        ]);
     }
 }
