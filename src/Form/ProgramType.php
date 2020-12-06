c8b012152cd65633b6e4b54432f8f6800cd659cb
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2020-12-06 21:39:05 +0100

    add checkboxes

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -6,6 +6,10 @@ use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Actor;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -15,6 +19,12 @@ class ProgramType extends AbstractType
             ->add('title')
             ->add('summary')
             ->add('poster')
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false])
             ->add('category', null, ['choice_label' => 'name'])
         ;
     }
